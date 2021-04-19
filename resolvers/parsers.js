e4eb433ce90687003585923e76e900d431a55f64
Filip Antala <fifcostyle@gmail.com>
2021-04-19 18:12:41 +0200

    added data parsers

---
+++ resolvers/parsers.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * @param {Array} data an array of the data to parse
+ */
+function playgroundDataParser(data) {
+	return data.map((val) => ({
+		id: val.playground_id,
+		name: val.playground_name,
+		description: val.playground_description,
+		location: {
+			address: {
+				formatted: val.playground_address,
+			},
+			geocoordinates: {
+				latitude: val.playground_latitude,
+				longtitude: val.playground_longtitude,
+			},
+			plus_code: val.playground_plus_code,
+			place_id: val.playground_place_id,
+		},
+		categories: JSON.parse(val.category_ids).map((id, i) => ({
+			id: id,
+			name: JSON.parse(val.category_names)[i],
+		})),
+		addedby: {
+			id: val.user_id,
+			username: val.user_username,
+			name: val.user_name,
+			email: val.user_email,
+			role: val.user_role,
+		},
+	}));
+}
+
+function categoryDataParser(data) {
+	return data.map(val => ({
+		id: val.category_id,
+		name: val.category_name,
+	}));
+}
+
+module.exports = { playgroundDataParser, categoryDataParser };
