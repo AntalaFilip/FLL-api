8d680deba4d69885d9d5c2f4e0f71c73efd50ff5
Filip Antala <fifcostyle@gmail.com>
2021-05-05 15:59:34 +0200

    quick dirty duplicate fix

--- resolvers/parsers.js
+++ resolvers/parsers.js
@@ -3,37 +3,46 @@
  * @param {Array} data an array of the data to parse
  */
 function playgroundDataParser(data) {
-	return data.map((val) => ({
-		id: val.playground_id,
-		name: val.playground_name,
-		description: val.playground_description,
-		location: {
-			address: {
-				formatted: val.playground_address,
+	return data.map((val) => {
+		const filteredCtgIds = Array.from(new Set(JSON.parse(val.category_ids)));
+		const filteredCtgNames = Array.from(new Set(JSON.parse(val.category_names)));
+		const filteredFacIds = Array.from(new Set(JSON.parse(val.facility_ids)));
+		const filteredFacNames = Array.from(new Set(JSON.parse(val.facility_names)));
+		console.log(val.playground_id, filteredCtgIds, 'f', filteredFacIds);
+		const obj = {
+			id: val.playground_id,
+			name: val.playground_name,
+			description: val.playground_description,
+			location: {
+				address: {
+					formatted: val.playground_address,
+				},
+				geocoordinates: {
+					latitude: val.playground_latitude,
+					longtitude: val.playground_longtitude,
+				},
+				plus_code: val.playground_plus_code,
+				place_id: val.playground_place_id,
 			},
-			geocoordinates: {
-				latitude: val.playground_latitude,
-				longtitude: val.playground_longtitude,
+			categories: filteredCtgIds.map((id, i) => ({
+				id,
+				name: filteredCtgNames[i],
+			})),
+			facilities: filteredFacIds.map((id, i) => ({
+				id,
+				name: filteredFacNames[i],
+			})),
+			addedby: {
+				id: val.user_id,
+				username: val.user_username,
+				name: val.user_name,
+				email: val.user_email,
+				role: val.user_role,
 			},
-			plus_code: val.playground_plus_code,
-			place_id: val.playground_place_id,
-		},
-		categories: JSON.parse(val.category_ids).map((id, i) => ({
-			id,
-			name: JSON.parse(val.category_names)[i],
-		})),
-		facilities: JSON.parse(val.facility_ids).map((id, i) => ({
-			id,
-			name: JSON.parse(val.facility_names)[i],
-		})),
-		addedby: {
-			id: val.user_id,
-			username: val.user_username,
-			name: val.user_name,
-			email: val.user_email,
-			role: val.user_role,
-		},
-	}));
+		};
+		console.log(obj);
+		return obj;
+	});
 }
 
 function categoryDataParser(data) {
